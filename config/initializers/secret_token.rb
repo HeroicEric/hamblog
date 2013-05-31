45801edcc6e1ea4fe6b04dce27badb8866776866
Eric Kelly <heroiceric@gmail.com>
2013-05-31 13:26:21 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hamlog::Application.config.secret_token = '8958d5ea6d86cc7ec18dc296f02dc997538a58b689fd39d456e4fdaff9446bb4771b290db502fea3fa3397c46f5f6af462638f02134669ca0c39cbacc40173e7'
